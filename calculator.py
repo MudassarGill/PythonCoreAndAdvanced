94e0fb5d65959573fd7bbeb5304f820f68e38b03
Mudassar Gill <mudassarhussain6533@gmail.com>
2026-03-14 21:03:17 -0700

    logging added

--- calculator.py
+++ calculator.py
@@ -1,79 +1,82 @@
-#Here we make a simple calculator using oop in python
 import logging
-class calculator:
 
-    #---------------- Logger Setup ---------------- #
- logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+class Calculator:
 
-# File handler
-file_handler = logging.FileHandler("calculator.log")
-file_handler.setLevel(logging.DEBUG)
+    # Logger setup
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
-# Console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("calculator.log")
+    console_handler = logging.StreamHandler()
 
-# Log format
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-# Add handlers
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
 
-logger.info("Preparing started")
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
-def __init__(self,num1,num2):
-        logger.info("Calculator initialized")
-        user_input_num1=int(input('Enter your first number'))
-        user_input_num2=int(input('Enter your second number'))
-        self.num1=user_input_num1
-        self.num2=user_input_num2
-def add(self):
-        logger.info("Addition performed")
-        print(f'The sum of {self.num1} and {self.num2} is {self.num1+self.num2}')
-        return self.num1+self.num2
-def sub(self):
-        logger.info("Subtraction performed")
-        print(f'The difference of {self.num1} and {self.num2} is {self.num1-self.num2}')
-        return self.num1-self.num2
-def mul(self):
-        logger.info("Multiplication performed")
-        print(f'The product of {self.num1} and {self.num2} is {self.num1*self.num2}')
-        return self.num1*self.num2
-def div(self):
-        logger.info("Division performed")
-        print(f'The division of {self.num1} and {self.num2} is {self.num1/self.num2}')
-        return self.num1/self.num2
+    logger.info("Calculator started")
 
-#main menu
+    def __init__(self):
+        self.num1 = int(input("Enter your first number: "))
+        self.num2 = int(input("Enter your second number: "))
+        self.logger.info("Numbers received")
+
+    def add(self):
+        result = self.num1 + self.num2
+        self.logger.info("Addition performed")
+        print(f"The sum of {self.num1} and {self.num2} is {result}")
+
+    def sub(self):
+        result = self.num1 - self.num2
+        self.logger.info("Subtraction performed")
+        print(f"The difference of {self.num1} and {self.num2} is {result}")
+
+    def mul(self):
+        result = self.num1 * self.num2
+        self.logger.info("Multiplication performed")
+        print(f"The product of {self.num1} and {self.num2} is {result}")
+
+    def div(self):
+        try:
+            result = self.num1 / self.num2
+            self.logger.info("Division performed")
+            print(f"The division of {self.num1} and {self.num2} is {result}")
+        except ZeroDivisionError:
+            print("Cannot divide by zero")
+            self.logger.error("Division by zero attempted")
+
 
 def main():
-    print("Welcome to the calculator")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Exit")
-    choice=int(input("Enter your choice: "))
-    if choice==1:
-        calc=calculator(0,0)
-        calc.add()
-    elif choice==2:
-        calc=calculator(0,0)
-        calc.sub()
-    elif choice==3:
-        calc=calculator()
-        calc.mul()
-    elif choice==4:
-        calc=calculator()
-        calc.div()
-    elif choice==5:
-        exit()
-    else:
-        print("Invalid choice")
+    while True:
+        print("\nWelcome to the Calculator")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 5:
+            print("Exiting calculator...")
+            break
+
+        calc = Calculator()
+
+        if choice == 1:
+            calc.add()
+        elif choice == 2:
+            calc.sub()
+        elif choice == 3:
+            calc.mul()
+        elif choice == 4:
+            calc.div()
+        else:
+            print("Invalid choice")
+
 
 if __name__ == "__main__":
     main()
